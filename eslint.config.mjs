cb0b146376ad8ce0b583feac7db4a7d5967d682f
animesh <phb21013@tezu.ac.in>
2025-04-19 01:15:45 +0530

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: [
+      'node_modules/',
+      '.next/',
+      'out/',
+      'src/generated/**'
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
